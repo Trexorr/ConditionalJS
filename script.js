976399e684fb3299531dd2522fdea570e53b4d34
Tristen Teixeira <trexorr@me.com>
2022-07-06 13:32:11 +1000

    first class

---
+++ script.js
@@ -0,0 +1,65 @@
+// If..else
+
+let breakfast = false
+let lunch = false
+let dinner = false
+
+// if (breakfast) {
+//     document.write(`<h1>Eggs and bacon</h1>`)
+//     lunch = false
+//     dinner = false
+// }
+
+// if (lunch) {
+//     document.write(`Steak and rice`)
+//     breakfast = false
+//     dinner = false
+// }
+
+// if (dinner) {
+//     document.write(`Stir fried noodle`)
+//     lunch = false
+//     breakfast = false
+// }
+
+let isRaining = false
+// if (isRaining) {
+//    document.write (`Take an umbrella`) 
+// } else {
+//     document.write (`Today is gonna be amazing`)
+// }
+
+// if (isRaining) {
+//     document.write(`<h1>Take an Umbrella<h2>`)
+// } else if (isRaining = false){
+//     document.write (`<h1>It's going to be a great day</h1>`)
+// }
+
+// isRaining ? document.write(`<h1>Take an Umbrella<h1>`) : document.write (`<h1>It's going to be a great day</h1>`)
+
+// switch(true){
+//     case breakfast:
+//         document.write(`Eggs and bacon`)
+//     break;
+//     case lunch:
+//         document.write(`Steak and fries`)
+//     break;
+//     case dinner:
+//         document.write(`Stirr Fried Rice`)
+//     default:
+//         document.write(`It's bed time, go to sleep.`)
+
+// }
+
+const firstName = ""
+
+switch(firstName){
+    case "Alice":
+        document.write(`Hey Alice!`)
+        break;
+    case "John":
+        document.write(`Hey John!`)
+        break;
+    default:
+        document.write(`Howdy Stranger..`)
+}
